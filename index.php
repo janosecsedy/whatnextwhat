f91b05bacf3b7eb9eb7f7936dcd6bcb62fff3a46
janos.ecsedy <janos.ecsedy@melkweg.hu>
2018-11-20 09:34:26 +0100

    yo

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-hello world
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Allerta+Stencil" rel="stylesheet">
+</head>
+<body style="text-align:center;font-family: 'Allerta Stencil', sans-serif; font-size:30px; padding-top:40vh;background-color:#f1f1f1;">
+WhatNext
+</body>
+</html>
